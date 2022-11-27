1b1117c61d8e3f81b3cbbbf14c67cec70f5dc398
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-27 23:51:08 +0100

    Aggiunge data dinamici in FooterTop

--- src/components/footer/FooterTop.vue
+++ src/components/footer/FooterTop.vue
@@ -1,6 +1,43 @@
 <script>
 export default {
     name: 'FooterTop',
+    data() {
+        return {
+            dcComicsList: [
+                'Characters',
+                'Comics',
+                'Movies',
+                'TV',
+                'Games',
+                'Videos',
+                'News'
+            ],
+            shopList: [
+                'Shop DC',
+                'Shop DC Collectibles'
+            ],
+            dcList: [
+                'Terms Of Use',
+                'Privacy policy (New)',
+                'Ad Choices',
+                'Advertising',
+                'Jobs',
+                'Subscriptions',
+                'Talent Workshops',
+                'CPSC Certificates',
+                'Ratings',
+                'Shop Help',
+                'Contact Us'
+            ],
+            sitesList: [
+                'DC',
+                'MAD Magazine',
+                'DC Kids',
+                'DC Universe',
+                'DC Power Visa'
+            ]
+        }
+    }
 }
 </script>
 
@@ -10,44 +47,23 @@ export default {
             <div class="col">
                 <h4>DC COMICS</h4>
                 <ul>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">News</a></li>
+                    <li v-for="item in dcComicsList" @click.prevent><a href="#">{{ item }}</a></li>
                 </ul>
                 <h4>SHOP</h4>
                 <ul>
-                    <li><a href="#">Shop DC</a></li>
-                    <li><a href="#">Shop DC Collectibles</a></li>
+                    <li v-for="item in shopList" @click.prevent><a href="#">{{ item }}</a></li>
                 </ul>
             </div>
             <div class="col">
                 <h4>DC</h4>
                 <ul>
-                    <li><a href="#">Terms Of Use</a></li>
-                    <li><a href="#">Privacy policy (New)</a></li>
-                    <li><a href="#">Ad Choices</a></li>
-                    <li><a href="#">Advertising</a></li>
-                    <li><a href="#">Jobs</a></li>
-                    <li><a href="#">Subscriptions</a></li>
-                    <li><a href="#">Talent Workshops</a></li>
-                    <li><a href="#">CPSC Certificates</a></li>
-                    <li><a href="#">Ratings</a></li>
-                    <li><a href="#">Shop Help</a></li>
-                    <li><a href="#">Contact Us</a></li>
+                    <li v-for="item in dcList" @click.prevent><a href="#">{{ item }}</a></li>
                 </ul>
             </div>
             <div class="col">
                 <h4>SITES</h4>
                 <ul>
-                    <li><a href="#">DC</a></li>
-                    <li><a href="#">MAD Magazine</a></li>
-                    <li><a href="#">DC Kids</a></li>
-                    <li><a href="#">DC Universe</a></li>
-                    <li><a href="#">DC Power Visa</a></li>
+                    <li v-for="item in sitesList" @click.prevent><a href="#">{{ item }}</a></li>
                 </ul>
             </div>
         </div>
